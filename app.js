abef3e6e7ec036729b88c101c96a977acf634328
Marilyn Balkaransingh <marilynbalkaransingh@marilyns-air.home>
2020-08-04 11:52:19 -0400

    initial frontend things

---
+++ app.js
@@ -0,0 +1,38 @@
+const deployedURL = first-app-bs23.herokuapp.com///mongodb+srv:hello:12345@cluster0.goaoh.mongodb.net/Quotes?retryWrites=true&w=majority
+const URL = deployedURL ? deployedURL : "http://localhost:3000";
+
+
+//GLOBAL VARIABLES
+const $nameInput = $("#createinput");
+const $quoteSelect = $("#createselect");
+const $button = $("#createbutton");
+const $quoteEditInput = $("#editinput");
+const $quoteEditSelect = $("#editselect");
+const $editButton = $("#editbutton");
+const $ul = $("ul");
+
+
+//FUNCTIONS
+
+//GET Quotes from API and populate
+
+//GET Movies from API and populate
+
+//CREATE New Quote
+
+//CREATE New Movie
+
+//UPDATE Quote
+
+//DELETE Quote
+
+//DELETE Movie
+
+
+
+
+//MAIN APPLICATION LOGIC
+
+
+
+
